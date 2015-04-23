ed25f02b7573a838aebca4765410d2c6b6a37822
David Bass <david.bass@wwu.edu>
2015-04-23 10:07:56 -0700

    created drupal.php

---
+++ drupal.php
@@ -0,0 +1,14 @@
+<?php 
+# put this code in your Drupal form, making sure that the #ids below match the fieldnames you used in your form
+
+$permalink = $_GET["permalink"];
+
+   drupal_add_js('
+
+     jQuery(document).ready(function(){ 
+          jQuery("#edit-submitted-openurl").val(document.referrer);
+          jQuery("#edit-submitted-page-record").val("' . $permalink .'");
+        });', 
+     'inline' 
+     );
+?>
