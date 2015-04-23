33eae019f9fc031dabf5fda40123e078daedcfd8
David Bass <david.bass@wwu.edu>
2015-04-23 10:19:16 -0700

    added todo

--- drupal.php
+++ drupal.php
@@ -2,6 +2,7 @@
 # put this code in your Drupal form, making sure that the #ids below match the fieldnames you used in your form
 
 $permalink = $_GET["permalink"];
+# TODO: sanitize $permalink 
 
    drupal_add_js('
 
